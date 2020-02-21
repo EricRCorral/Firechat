abd60090843abb5b9fac8e1b3802dea7b58e2870
EricRCorral <eric-wil@hotmail.com>
2020-02-21 17:02:21 -0300

    Uploading deployable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCtUFsltjnj6JYwdrRy6YnR2Wiz3qR1U44',
+    authDomain: 'firechat-e9ad4.firebaseapp.com',
+    databaseURL: 'https://firechat-e9ad4.firebaseio.com',
+    projectId: 'firechat-e9ad4',
+    storageBucket: 'firechat-e9ad4.appspot.com',
+    messagingSenderId: '892614047571',
+    appId: '1:892614047571:web:0ffbf80bcdbc5fee4613a6',
+    measurementId: 'G-XQKZY8GJMS'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
-    apiKey: "AIzaSyCtUFsltjnj6JYwdrRy6YnR2Wiz3qR1U44",
-    authDomain: "firechat-e9ad4.firebaseapp.com",
-    databaseURL: "https://firechat-e9ad4.firebaseio.com",
-    projectId: "firechat-e9ad4",
-    storageBucket: "",
-    messagingSenderId: "892614047571",
-    appId: "1:892614047571:web:0ffbf80bcdbc5fee4613a6",
-    measurementId: "G-XQKZY8GJMS"
+    apiKey: 'AIzaSyCtUFsltjnj6JYwdrRy6YnR2Wiz3qR1U44',
+    authDomain: 'firechat-e9ad4.firebaseapp.com',
+    databaseURL: 'https://firechat-e9ad4.firebaseio.com',
+    projectId: 'firechat-e9ad4',
+    storageBucket: 'firechat-e9ad4.appspot.com',
+    messagingSenderId: '892614047571',
+    appId: '1:892614047571:web:0ffbf80bcdbc5fee4613a6',
+    measurementId: 'G-XQKZY8GJMS'
   }
 };
 
